45ac03573644b4737f2427ec3224da21a6be2380
fareska <fares.xb@gmail.com>
2020-11-08 14:31:51 +0200

    first update for the tweeter project

--- Tweeter/main.js
+++ Tweeter/main.js
@@ -3,42 +3,35 @@ const tweeter = Tweeter()
 const renderer = Renderer()
 
 renderer.renderPost(tweeter.getPosts())
- 
-    // return {
-    //     getPosts: getPosts,
-    //     addPost: addPost,
-    //     removePost: removePost,
-    //     addComment: addComment,
-    //     removeComment: removeComment,
-    // }
 
-const addThePost = () => $("#input").val()
-
-const removeTheCommentPId = () => closest(".post").id  
-const removeTheCommentCId = () => closest(".comments").data().id
-
-
-
-
-const post = () => {                   ///// there is already onClick attr on the HTML file
-    tweeter.addPost("addThePost")
+/////add post- twit button
+const post = () => {
+    tweeter.addPost($("#input").val())
     $("#input").val("")
+    renderer.renderPost(tweeter.getPosts())
 }
 
+//////remove post - delete post button
+$("#posts").on("click", ("button.delete"), function () {
+    tweeter.removePost($(this).closest(".post").attr("id"))
+    renderer.renderPost(tweeter.getPosts())
 
+})
 
-$("#posts").on("click", ".post-text", function(){    // removePost
-    tweeter.removePost((this).closest(".post").id) 
+/////add comment - comment button
+$("#posts").on("click", "button.cButton", function () {
+    let cText = $(this).closest(".post").find(".cInput").val()
+    let postId = $(this).closest(".post").attr("id")
+    tweeter.addComment(cText, postId)
+    renderer.renderPost(tweeter.getPosts())
+})
+
+////// remove comment - the X button
+$("#posts").on("click", ".delete-comment", function () {
+    tweeter.removeComment($(this).closest(".post").attr("id"), $(this).closest(".comments").data("id"))
+    renderer.renderPost(tweeter.getPosts())
 })
 
 
-$("#posts").on("click", ".delete-comment", function(){    // removeComment
-    tweeter.removeComment($(this).removeTheCommentPId(), $(this).removeTheCommentCId()).closest(".comments")
-})
 
 
-
-// $("#input").on("click", function(){
-//         tweeter.addPost(addThePost);
-//         $("#input").val("")
-// })
--- Tweeter/render.js
+++ Tweeter/render.js
@@ -1,38 +1,36 @@
 
 
 
-const Renderer = function (){
+const Renderer = function () {
 
-const renderPost = function (posts) {
-    
-    $("#posts").empty()
-    
-    let postText = ""
-    let postId = ""
+    const renderPost = function (posts) {
 
-    for (let post of posts){ 
-        postText = post.text
-        postId = post.id   //// ask if i should convert the string id to a number
-        
-        postDiv = $(`<div class="post" id = "${postId}"data-id="${parseInt(postId.substring(1))}">
+        $("#posts").empty()
+
+        let postText = ""
+        let postId = ""
+
+        for (let post of posts) {
+            postText = post.text
+            postId = post.id
+
+            postDiv = $(`
+        <div class="post" id = "${postId}">
         <br> 
         <h5 class="post-text">${postText}</h5>       
-        <input type="text" placeholder="Got something to say?"> <button>Comment</button>
+        <input type="text" placeholder="Got something to say?" class="cInput"></input>
+        <button class="cButton">Comment</button>
         <button class="delete" >Delete Post</button>
         </div> `)
-        $("#posts").append(postDiv) 
+            $("#posts").append(postDiv)
 
-        for (let comment of post.comments){
-            const commentDive = $(`<div class="comments" data-id="${comment.id}">
+            for (let comment of post.comments) {
+                const commentDive = $(`<div class="comments" data-id="${comment.id}">
                                  <button class="delete-comment">X</button> ${comment.text}
                                  </div> `)
-            $(`#${postId}`).find(".post-text").append(commentDive)
+                $(`#${postId}`).find(".post-text").append(commentDive)
+            }
         }
-    
     }
-
-} 
-
-return {renderPost:renderPost}
-
+    return { renderPost: renderPost }
 }
